99d35393c46336084f1af4edc0375efef30c5113
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-10-06 20:19:18 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
 const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+    { url: "https://picsum.photos/id/237/200/300" },
+    { url: "https://picsum.photos/id/238/200/300" },
+    { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+function downloadImages(imageArray) {
+    const promises = imageArray.map(image => {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.src = image.url;
+
+            img.onload = () => resolve(img);
+            img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+        });
+    });
+
+    return Promise.all(promises);
+}
+
+
+btn.addEventListener("click", () => {
+    output.innerHTML = ''; 
+
+    downloadImages(images)
+        .then((loadedImages) => {
+            loadedImages.forEach(img => {
+                output.appendChild(img);
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            output.innerHTML = `<div class="alert alert-danger">${error}</div>`;
+        });
+});
